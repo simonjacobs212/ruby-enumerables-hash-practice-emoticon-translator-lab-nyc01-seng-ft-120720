1d0b1f418ef5ed421a11dbf1e7fca4538bf3cd5a
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-11-20 16:29:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,7 +26,7 @@ end
 def get_english_meaning(path, emoticon)
   emoticons = load_library(path)
   emoticons.each do |key, language_set|
-    if value.include?(emoticon)
+    if language_set.include?(emoticon)
       return key
     end
   end
