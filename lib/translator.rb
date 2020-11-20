7471140e6e70c383751a0af50c77e6379f35ff14
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-11-20 16:12:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,6 +23,8 @@ def get_japanese_emoticon
   # code goes here
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(path, emoticon)
+  emoticons = YAML.load_file(path)
+  binding.pry
 end
+  # code goes here
